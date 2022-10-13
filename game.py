8233a045ccb9151cd8df6de63cb3eadd2aee8d70
Ry-Lowe <115638624+Ry-Lowe@users.noreply.github.com>
2022-10-13 09:42:21 -0400

    ship window

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 import pygame
 import sys
-
+from settings import Settings
+from ship import Ship
 class AlienInvasion:
     def __init__(self):
-        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 if __name__ == '__main__':
     ai = AlienInvasion()
